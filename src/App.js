8959f27ac14cd0fab01713b5ca3eff46fd5f71bd
Ahd <dafuq.ahd@gmail.com>
2020-04-13 03:43:16 +0300

    add 3rd section to landing page

--- src/App.js
+++ src/App.js
@@ -9,8 +9,12 @@ import dress3 from "./img/content/img3.jpg";
 import abaya1 from "./img/content/abaya5.jpg";
 import abaya2 from "./img/content/abaya6.jpg";
 import abaya3 from "./img/content/abaya7.jpg";
+import coat1 from "./img/content/img5.jpg";
+import coat2 from "./img/content/img6.jpg";
+import coat3 from "./img/content/img4.jpg";
 const dummy = [dress1, dress2, dress3];
 const dummy2 = [abaya1, abaya2, abaya3];
+const dummy3 = [coat1, coat2, coat3];
 function App() {
   return (
     <div className="App">
@@ -29,6 +33,13 @@ function App() {
         text={{
           first_word: "STYLISH",
           second_word: "ABAYA",
+        }}
+      />
+      <Carousel
+        data={dummy3}
+        text={{
+          first_word: "ELEGANT",
+          second_word: "COATS",
         }}
       />
     </div>
